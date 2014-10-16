e06cc3ac4835f3042bed96e36a900ed3d397d700
Vegard Haugstvedt <it.vegard@gmail.com>
2014-10-16 17:04:48 +0200

    Created separate method for printing results

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,7 @@
 var actionUrl = "#";
 
+var dummyData = ["Anders", "Vegard", "Runar"];
+
 $(document).ready(function() {
 
     $.fn.extend({
@@ -10,13 +12,21 @@ $(document).ready(function() {
                 dataType: "json",
                 data: skill
             }).done(function(data){
-                for(var i = 0; i < data.length; i++) {
-                    var person = data[i];
-                    $("#resultElement").append("li").append(person);
-                }
+                $.printResult(dummyData, skill);
             });
             return false;
+        },
+        
+        printResult: function(data, skillName) {
+            $("#resultElement").empty();
+            for(var i = 0; i < data.length; i++) {
+                var person = data[i];
+                $("#skillName").text(skillName);
+                $("#resultElement").append("<li>" + person + "</li>");
+            }
         }
     })
+    
+    //$("body").printResult(dummyData, "neo4j");
 
 });
