97b3ec9304e9366bdbce0c93465495de81b968a1
John Alexander Bye <john.alexander.bye@itera.no>
2014-10-16 17:35:15 +0200

    Initial implementation of PersonController

--- backend/src/main/java/no/itera/skilldb/controller/PersonController.java
+++ backend/src/main/java/no/itera/skilldb/controller/PersonController.java
@@ -1,5 +1,13 @@
 package no.itera.skilldb.controller;
 
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import no.itera.skilldb.domain.Person;
+import no.itera.skilldb.domain.Skill;
+import no.itera.skilldb.repositories.PersonRepository;
+import no.itera.skilldb.repositories.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +18,40 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PersonController {
 
-    @RequestMapping("/javabeer")
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    SkillRepository skillRepository;
+
+    @RequestMapping("/insert")
     public
     @ResponseBody
-    String beer() {
-        return "";
+    String insert() {
+        Gson gson = new Gson();
+
+        skillRepository.deleteAll();
+        personRepository.deleteAll();
+
+        Skill s = new Skill("java");
+        Skill s2 = new Skill("neo4j");
+        skillRepository.save(s);
+        skillRepository.save(s2);
+
+        Person p = new Person("John", "Bye");
+        p.addSkill(s);
+        p.addSkill(s2);
+
+        personRepository.save(p);
+        return "OK";
+    }
+
+    @RequestMapping("/people")
+    public
+    @ResponseBody
+    String list() {
+        Gson gson = new Gson();
+        Person person = personRepository.findByFirstName("John");
+        return gson.toJson(person);
     }
 
 }
